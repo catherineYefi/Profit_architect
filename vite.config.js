efc0eae1f9b69daa0db57d9dc316a268e7b514ff
catherineYefi <catherine0077@gmail.com>
2026-04-02 14:36:01 +0000

    fix: correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // TODO: замени на имя своего репозитория на GitHub
-  base: '/profit-architect/',
+  base: '/Profit_architect/',
 })
